7716139e854e6990abef627f00779c3301806551
moonsea16 <chenyujen007@gmail.com>
2026-03-14 21:28:53 +0800

    Update url.js

--- url.js
+++ url.js
@@ -7,9 +7,18 @@
  */
 
 
+/*
+ * 將要隨機派發的網址放入下方（不需排序）
+ *
+ * 並請注意：
+ * 1. 網址請用引號（或稱「撇號」，單引號或雙引號皆可）包起來
+ * 2. 包起來的網址之間用逗號分隔
+ */
+
+
 const urls = [
-	'https://forms.gle/rdvsDuPX9RbGCuz98',
-	'https://forms.gle/EmYVWukfsZhyn7DR7',
-	'https://forms.gle/y1FRQvgJjr4bB5My6',
-	'https://forms.gle/oqmPn2ybd5SiiAKb7'
+	'https://forms.gle/sDEE3doK3yRpxDzw5',
+	'https://forms.gle/YRwNe8cfkYhSkhek6',
+	'https://forms.gle/ykLQ2a3FhsY5xJSy7',
+	'https://forms.gle/sD1npGeoXU7p81eK9'
 ];
