e88ec9d1faaf1ef2fa25ba7cd5ca4985d13b3ad9
moonsea16 <chenyujen007@gmail.com>
2026-03-17 16:36:04 +0800

    Update url.js

--- url.js
+++ url.js
@@ -11,9 +11,9 @@
 
 const urls = [
 	'https://forms.gle/Jsdupvej4LVK3Yyz7',
-	'https://forms.gle/wVhNMgvWuGpxPvUt7',
+	'https://forms.gle/xWY6U33QD2VkduAq8',
 	'https://forms.gle/x8ivwYEz6UV7pRvw5',
 	'https://forms.gle/abScC1Qs5vue4GiU7',
-	'https://forms.gle/wVhNMgvWuGpxPvUt7',
+	'https://forms.gle/xWY6U33QD2VkduAq8',
 	'https://forms.gle/x8ivwYEz6UV7pRvw5'
 ];
